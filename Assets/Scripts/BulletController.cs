51dd1c1c39c080a22fdc20a3e1b4305be36d6571
DasArmin <armin.hunter@hotmail.com>
2016-09-13 11:53:59 +0200

    Added basic ability to charge and shoot.

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -4,14 +4,23 @@ using System.Collections;
 public class BulletController : MonoBehaviour
 {
     [SerializeField]
+    private Rigidbody2D rbBullet;
+    [SerializeField]
     private CircleCollider2D destructionCircle;
     [SerializeField]
     private GroundController groundController;
+
+    public void DirectBullet(Vector2 dir, float power)
+    {
+        power = power * 0.25f;
+        rbBullet.AddForce(new Vector2(dir.x * power, dir.y * power), ForceMode2D.Impulse);
+    }
 
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.collider.CompareTag("Ground"))
         {
+            groundController = coll.gameObject.GetComponent<GroundController>();
             groundController.DestroyGround(destructionCircle);
             Destroy(this.gameObject);
         }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,11 +3,56 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // The amount of force we will apply to our bullet.
     [SerializeField]
-    private float movementSpeed;
+    private float shootPower;
+    // The bullets that are given to us in our level. 
+    [SerializeField]
+    private BulletController[] bullets;
+    // This number will help us indicate which index we are in the bullets array.
+    private int bulletNum = 0;
+    // Check if we have shot a bullet.
+    [SerializeField]
+    private bool hasShot = false;
+    [SerializeField]
+    private Vector3 shootDirection;
 
 	void Update ()
     {
-	    
-	}
+        // Setting our shootDirection equal to our mouse position.
+        shootDirection = Input.mousePosition;
+        shootDirection = Camera.main.ScreenToWorldPoint(shootDirection);
+        shootDirection = shootDirection - transform.position;
+
+        // Shoot function
+        if (!hasShot && Input.GetMouseButton(0))
+        {
+            // Increase the amount of power the longer we hold our mouse button.
+            StartCoroutine(IncreasePower());
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            StopAllCoroutines();
+            ShootBullet();
+        }
+    }
+
+    IEnumerator IncreasePower()
+    {
+        while (true)
+        {
+            shootPower++;
+            print("POWER UP!!");
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void ShootBullet()
+    {
+        shootPower = Mathf.Clamp(shootPower, 1f, 20);
+        BulletController bulletInstance = Instantiate(bullets[bulletNum], transform.position, Quaternion.Euler(Vector3.zero)) as BulletController;
+        bulletInstance.DirectBullet(shootDirection, shootPower);
+        //bulletNum++;
+        shootPower = 0;
+    }
 }
