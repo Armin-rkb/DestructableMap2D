27431b29cb9d6a85dc08b36a0b8d878a82402692
DasArmin <armin.hunter@hotmail.com>
2016-09-13 20:56:21 +0200

    Added new Cursor and body rotation(needs art)

---
+++ Assets/Scripts/ChangeCursor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeCursor : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite cursorSprite;
+
+    void Start()
+    {
+        Cursor.SetCursor(cursorSprite.texture, Vector2.zero, CursorMode.Auto);
+    }
+
+}
---
+++ Assets/Scripts/RotateBody.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateBody : MonoBehaviour
+{
+    
+	void Update ()
+    {
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 dir = Input.mousePosition - pos;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
